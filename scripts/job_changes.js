7f7d8c0ae4816119388c56dd1f40210fba0ebc58
Brian O'Raghallaigh <oraghab@ad.dcu.ie>
2019-08-06 14:54:35 +0100

    Update job_changes.js

--- scripts/job_changes.js
+++ scripts/job_changes.js
@@ -1,12 +1,13 @@
-var FROM="2019-05-01"; //on or after this date
-var TILL="2019-07-01"; //before this date
+var FROM="2019-06-26"; //on or after this date
+var TILL="2019-08-01"; //before this date
 const WRITE_TO="_entrylist.txt";
 
 //------
 
 const fs=require("fs");
 const Database = require('better-sqlite3');
-const db = new Database('../data/termbases/bnt.sqlite', { fileMustExist: true });
+//const db = new Database('../data/termbases/bnt.sqlite', { fileMustExist: true });
+const db = new Database('bnt-2019-08-01_2300.sqlite', { fileMustExist: true });
 
 fs.writeFileSync(WRITE_TO, "");
 db.prepare(`select h.id, h.[when], h.email, h.entry_id, h.json as json_new, (
