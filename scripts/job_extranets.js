b036d26e93eaed83f72714ebebdff151ef5912ce
Brian O'Raghallaigh <oraghab@ad.dcu.ie>
2019-12-06 10:40:09 +0000

    Create job_extranets.js

---
+++ scripts/job_extranets.js
@@ -0,0 +1,36 @@
+//set extranets=["4626859"] for each entry in _entrylist.txt
+
+const BNT_EXTRANET_ID="4626859"; // An t-eislíon in BNT (Terminologue) air ar cheart na hiontrálacha a chur.
+
+var entryIDs=[];
+
+var lineReader=require('readline').createInterface({
+  input: require('fs').createReadStream('./_entrylist.txt')
+});
+lineReader.on('line', function(line) {
+  var entryID=line.split("\t")[0];
+  entryIDs.push(parseInt(entryID));
+});
+lineReader.on("close", function(){
+  onebyone();
+});
+
+function onebyone(){
+  const Database = require('better-sqlite3');
+  const db = new Database('../data/termbases/bnt.sqlite', { fileMustExist: true });
+  const selEntry = db.prepare('select * from entries where id=?');
+  const updEntry = db.prepare('update entries set json=? where id=?');
+  entryIDs.map(entryID => {
+    var rowEntry=selEntry.get(entryID);
+    if(!rowEntry){
+      console.log(`${entryID}: no need to do`);
+    } else {
+      var entry=JSON.parse(rowEntry.json);
+      entry.extranets=[BNT_EXTRANET_ID];
+      var json=JSON.stringify(entry);
+      var info=updEntry.run(json, entryID);
+      console.log(`${entryID}: done, rows changed: ${info.changes}`);
+    }
+  });
+  db.close();
+}
