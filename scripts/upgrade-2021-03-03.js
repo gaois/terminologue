c66030588ba1af500e03b3ebc3804e8f0f715be8
Ronan Doherty <ronan.odochartaigh@gmail.com>
2021-03-03 14:24:22 +0000

    Innéacs breise ar mhaithe le cuardaigh ilfhoclacha

---
+++ scripts/upgrade-2021-03-03.js
@@ -0,0 +1,24 @@
+//A script to migrate all SQLite databases when the database schema has changed.
+
+const path=require("path");
+const fs=require("fs");
+const Database=require('better-sqlite3');
+
+//migrateDBs(path.join(__dirname, "../website/termbaseTemplates/"));
+migrateDBs(path.join(__dirname, "../data/termbases/"));
+
+function migrateDBs(dirPath){
+    var filenames=fs.readdirSync(dirPath).filter(filename => /\.sqlite$/.test(filename));
+    var count=filenames.length;
+    console.log(`${count} databases to do`);
+    filenames.map(filename => {
+      console.log(`beginning ${filename}`);
+      var filepath=path.join(dirPath, filename);
+      const db = new Database(filepath, { fileMustExist: true });
+  
+      db.prepare(`CREATE INDEX IF NOT EXISTS ix_words_term_id_word ON words (term_id, word);`).run();
+  
+      db.close();
+      console.log(` - done ${filename}, ${--count} databases remaining`);
+    });
+  }
