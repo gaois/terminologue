cc63f90de828f46efd8f9c33d6bbb5e9a2ed4929
Brian Ó Raghallaigh <brian.oraghallaigh@dcu.ie>
2024-05-17 12:55:36 +0100

    Update job_changes.js

--- scripts/job_changes.js
+++ scripts/job_changes.js
@@ -1,13 +1,13 @@
-var FROM="2024-03-01"; //on or after this date
-var TILL="2024-04-01"; //before this date
-const WRITE_TO="2024-04-01_entrylist.txt";
+var FROM="2024-04-01"; //on or after this date
+var TILL="2024-05-01"; //before this date
+const WRITE_TO="2024-05-01_entrylist.txt";
 
 //------
 
 const fs=require("fs");
 const Database = require('better-sqlite3');
 //const db = new Database('../data/termbases/bnt.sqlite', { fileMustExist: true });
-const db = new Database('C:/Users/oraghab/Documents/BOR/zzz/bnt/bnt-2024-04-01_1954.sqlite', { fileMustExist: true });
+const db = new Database('C:/Users/oraghab/Documents/BOR/zzz/bnt/bnt-2024-05-09_1221.sqlite', { fileMustExist: true });
 
 fs.writeFileSync(WRITE_TO, "");
 db.prepare(`select h.id, h.[when], h.email, h.entry_id, h.json as json_new, (
